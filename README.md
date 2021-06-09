ee478e57d60c55a6d720f149baeec29c8c85540d
IgnacioGR <ignagilroca@gmail.com>
2021-06-09 18:47:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DiceGame
+<p>Refresh the page to play the game</p>
+<a href="https://ignaciogr.github.io/DiceGame/">Live Demo</a>
---
+++ roll.js
@@ -0,0 +1,24 @@
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+function winner() {
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "It is a draw";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML =
+      '<i class="fa fa-flag" aria-hidden="true"></i> Player1 wins';
+  } else {
+    document.querySelector("h1").innerHTML =
+      'Player2 wins <i class="fa fa-flag" aria-hidden="true"></i>';
+  }
+}
+
+winner();
